9e2946faa46f43e21d9f6e04fc1b2fcfac1b7dfa
cocaine_coder <tracywang1997@outlook.com>
2024-06-04 17:47:02 +0800

    update

--- components/gis/Maplibre.vue
+++ components/gis/Maplibre.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="map" :style="{height:height ?? '500px',margin:'10px 0'}">
+    <div id="map" :style="{ height: height ?? '500px', margin: '10px 0' }">
     </div>
     <slot v-if="loaded" :map="map"></slot>
 </template>
@@ -7,7 +7,7 @@
 <script setup lang="ts">
 import maplibregl from 'maplibre-gl';
 import "maplibre-gl/dist/maplibre-gl.css"
-import { onMounted,onUnmounted ,ref} from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 
 const props = defineProps<{
     height?: string,
@@ -21,7 +21,31 @@ const loaded = ref(false);
 onMounted(() => {
     map = new maplibregl.Map({
         container: 'map',
-        style: "https://demotiles.maplibre.org/style.json",
+        style: {
+            "zoom": 1,
+            "pitch": 0,
+            "center": [
+                120,
+                31
+            ],
+            "glyphs": "https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf",
+            "layers": [{
+                id: "esri_satellite",
+                type: "raster",
+                source: "esri_satellite",
+            }],
+            "bearing": 0,
+            "sources": {
+                "esri_satellite": {
+                    type: 'raster',
+                    tiles: ["https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"],
+                    maxzoom: 18
+                }
+            },
+            "version": 8,
+            "metadata": {
+            }
+        },
     });
 
     props.onCreated?.call(undefined, map);
@@ -32,7 +56,7 @@ onMounted(() => {
     });
 });
 
-onUnmounted(()=>{
+onUnmounted(() => {
     map.remove();
 })
 
